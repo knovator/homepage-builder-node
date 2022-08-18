13d0f150a06880c305b8f425b9bcaddf1596b8f5
chavda-bhavik <bhavikvchavda@gmail.com>
2022-08-18 12:01:56 +0530

    Added descriptors for permissions

--- src/routes/WidgetRoute.ts
+++ src/routes/WidgetRoute.ts
@@ -1,32 +1,34 @@
 import express from 'express';
+require('express-list-endpoints-descriptor')(express);
 
 import validate from '../utils/validate';
 import * as widgetValidation from '../utils/validations/widget';
 import * as widgetController from '../controllers/WidgetController';
+import { IRouter } from '../Router';
 
-const routes = express.Router();
+const routes: IRouter = express.Router();
 routes.use(express.json());
 
 // Widget Routes
 // Get all widgets
-routes.post(
-	`/list`,
-	validate(widgetValidation.list),
-	widgetController.getWidgets
-);
+routes
+	.post(`/list`, validate(widgetValidation.list), widgetController.getWidgets)
+	.descriptor('widget.getAll');
 // Create a widget
-routes.post(
-	`/`,
-	validate(widgetValidation.create),
-	widgetController.createWidget
-);
+routes
+	.post(`/`, validate(widgetValidation.create), widgetController.createWidget)
+	.descriptor('widget.create');
 // Update a widget
-routes.put(
-	`/:id`,
-	validate(widgetValidation.update),
-	widgetController.updateWidget
-);
+routes
+	.put(
+		`/:id`,
+		validate(widgetValidation.update),
+		widgetController.updateWidget
+	)
+	.descriptor('widget.update');
 // Delete a widget
-routes.delete(`/:id`, widgetController.deleteWidget);
+routes
+	.delete(`/:id`, widgetController.deleteWidget)
+	.descriptor('widget.delete');
 
 export default routes;
